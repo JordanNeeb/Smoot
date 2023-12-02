f8940096a97b6bad4976e9604f30bee77210dcf2
Jordan <jordan.neeb@bronsontelecom.ca>
2023-12-02 15:58:16 -0400

    adds basic mm to feet converter

---
+++ app/components/form.js
@@ -0,0 +1,82 @@
+"use client";
+
+import React, { useState } from "react";
+import { ArrowRight, ChevronDown } from "feather-icons-react";
+
+const Form = () => {
+  const [measurment, setMeasurment] = useState(null);
+  const [conversion, setConversion] = useState({
+    feet: null,
+    inches: null,
+    numerator: null,
+    denominator: null,
+  });
+
+  const convert = () => {
+    const float = measurment * 0.0393701;
+    const whole = Math.floor(float);
+    const remainder = (float % 1).toFixed(4);
+    const feet = Math.floor(whole / 12);
+    const inches = whole - feet * 12;
+    let numerator = Math.round(remainder * 16);
+    let denominator = 16;
+
+    console.log(float);
+    console.log(remainder);
+    console.log(numerator);
+
+    while (numerator % 2 == 0) {
+      numerator = numerator / 2;
+      denominator = denominator / 2;
+    }
+
+    console.log(numerator);
+    console.log(denominator);
+
+    setConversion({ feet, inches, numerator, denominator });
+  };
+
+  return (
+    <div className="flex flex-col justify-center h-full">
+      <div className="flex justify-center text-5xl text-cyan-800 mb-10 h-12">
+        {`${conversion.feet ? `${conversion.feet}'` : ""}`}
+        <span>&nbsp;</span>
+        {`${conversion.inches ? conversion.inches : ""}`}
+        {conversion.numerator && (
+          <span className="text-3xl">
+            <sup>{conversion.numerator}</sup>&frasl;
+            <sub>{conversion.denominator}</sub>
+          </span>
+        )}
+        {`${conversion.numerator || conversion.feet ? '"' : ""}`}
+      </div>
+      <div>
+        <label
+          for="measurment"
+          class="block mb-2 pl-0.5 text-xs font-bold text-gray-500 dark:text-white uppercase"
+        >
+          Measurment
+        </label>
+        <input
+          type="number"
+          id="measurment"
+          class=" mb-4 bg-gray-50 border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-cyan-500 dark:focus:border-cyan-500"
+          onChange={(e) => setMeasurment(e.target.value)}
+        />
+      </div>
+      <div className="flex flex-row justify-around font-bold text-gray-500 text-xs uppercase items-center">
+        <span>millimeters</span>
+        <ArrowRight />
+        <span>feet</span>
+      </div>
+      <button
+        onClick={convert}
+        className="mt-10 block w-full p-2.5 bg-cyan-800 text-white uppercase rounded-lg text-sm font-medium"
+      >
+        Convert
+      </button>
+    </div>
+  );
+};
+
+export default Form;
