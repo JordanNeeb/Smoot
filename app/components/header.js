1739739695f555584524f290c140c6a78543a7b6
Jordan <jordan.neeb@bronsontelecom.ca>
2023-12-02 10:28:13 -0400

    adds basic dropdown

--- app/components/header.js
+++ app/components/header.js
@@ -1,11 +1,28 @@
-import { Settings } from "feather-icons-react";
+"use client";
+
+import React, { useState } from "react";
+import { Anchor, Settings } from "feather-icons-react";
 
 const Header = () => {
+  const [settingsIsOpen, setSettingsIsOpen] = useState(false);
+
   return (
-    <div className="flex flex-row justify-between items-start">
-      <div>Logo</div>
+    <div className="flex flex-row justify-between items-start text-sky-400">
       <div>
-        <Settings />
+        <Anchor />
+      </div>
+      <div className="relative">
+        <Settings onClick={() => setSettingsIsOpen(!settingsIsOpen)} />
+        {settingsIsOpen ? (
+          <ul className="absolute">
+            <li className="menu-item">
+              <button>Menu 1</button>
+            </li>
+            <li className="menu-item">
+              <button>Menu 2</button>
+            </li>
+          </ul>
+        ) : null}
       </div>
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Header from "./components/header";
 
 const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-6">
+    <main className="flex min-h-screen flex-col justify-between p-6 bg-gray-900">
       <Header />
     </main>
   );
